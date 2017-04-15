5c8eb1c488ff38323f53d882fecfde456386a15e
Timothy D <timothydowns@gmail.com>
2017-04-15 13:47:06 +1000

    Update sql.lua

--- src/lib/sql.lua
+++ src/lib/sql.lua
@@ -1,19 +1,21 @@
 _M = {}
 
-_M.fmt = function(db, sql, arg1, arg2, arg3)
+_M.query = function(db, sql, arg1, arg2, arg3)
+  
   local res
   local ok, err, errcode, sqlstate = db:connect{
     host = "127.0.0.1",
     port = 3306,
-    database = "ngx_test",
-    user = "ngx_test",
-    password = "ngx_test",
+    database = "test",
+    user = "tim",
+    password = "",
     max_packet_size = 1024 * 1024 }
 
   if not ok then
     ngx.log(ngx.ERR, "failed to connect: " .. tostring(err) .. ": " .. tostring(errcode) .. " " tostring(sqlstate))
     return "err_db_connection_lost", nil
   end
+  
   local fmted_sql = string.format(sql, 
     arg1 and ngx.quote_sql_str(arg1),
     arg2 and ngx.quote_sql_str(arg2),
